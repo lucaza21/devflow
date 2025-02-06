c84292699ec1cb88da3982f86090e8ed0d6ab73e
obiito21 <luis.carrasquilla.z@gmail.com>
2025-02-06 18:12:39 -0500

    Tailwind & Fonts setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl   font-black text-black">
-      Welcome to the world of Next.js
-    </h1>
+    <>
+      <div className=" bg-white  dark:bg-green-200 dark:text-green-300">
+        <h1 className="h1-bold font-inter">Welcome to the world of Next.js </h1>
+      </div>
+      <div className="background-light850_dark100">
+        <h1 className="h1-bold font-spaceGrotesk">
+          Welcome to the world of Next.js
+        </h1>
+      </div>
+    </>
   );
 }
